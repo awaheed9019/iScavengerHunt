549b35507e5851f5be0bff21cec39ed85c3b1538
Anam Waheed <a.waheed@nyu.edu>
2016-10-08 19:33:28 -0400

    first page

--- app/src/main/java/com/example/computer/hackny/MainActivity.java
+++ app/src/main/java/com/example/computer/hackny/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.computer.hackny;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
